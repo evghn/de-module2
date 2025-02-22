827095b71fb8a1493f8f9c57596a46d128fed5ac
evghn <evghn94_git@mail.ru>
2025-02-22 12:29:34 +0300

    Оформление главной

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'name' => 'Мой Не Сам',
     'language' => 'ru-RU',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
