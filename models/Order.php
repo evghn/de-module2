0d45b0dc7b4f3b651e2ee3d1eabf6088d44afda3
evghn <evghn94_git@mail.ru>
2025-02-22 09:49:29 +0300

    Генерация моделей

---
+++ models/Order.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "order".
+ *
+ * @property int $id
+ * @property string $address
+ * @property string $contact
+ * @property string $created_at
+ * @property string $date
+ * @property string $time
+ * @property int $service_id
+ * @property int $pay_type_id
+ * @property int $status_id
+ * @property int $user_id
+ * @property string|null $reason
+ *
+ * @property PayType $payType
+ * @property Service $service
+ * @property Status $status
+ * @property User $user
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'order';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['address', 'contact', 'date', 'time', 'service_id', 'pay_type_id', 'status_id', 'user_id'], 'required'],
+            [['created_at', 'date', 'time'], 'safe'],
+            [['service_id', 'pay_type_id', 'status_id', 'user_id'], 'integer'],
+            [['address', 'contact', 'reason'], 'string', 'max' => 255],
+            [['pay_type_id'], 'exist', 'skipOnError' => true, 'targetClass' => PayType::class, 'targetAttribute' => ['pay_type_id' => 'id']],
+            [['service_id'], 'exist', 'skipOnError' => true, 'targetClass' => Service::class, 'targetAttribute' => ['service_id' => 'id']],
+            [['status_id'], 'exist', 'skipOnError' => true, 'targetClass' => Status::class, 'targetAttribute' => ['status_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'address' => 'Address',
+            'contact' => 'Contact',
+            'created_at' => 'Created At',
+            'date' => 'Date',
+            'time' => 'Time',
+            'service_id' => 'Service ID',
+            'pay_type_id' => 'Pay Type ID',
+            'status_id' => 'Status ID',
+            'user_id' => 'User ID',
+            'reason' => 'Reason',
+        ];
+    }
+
+    /**
+     * Gets query for [[PayType]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPayType()
+    {
+        return $this->hasOne(PayType::class, ['id' => 'pay_type_id']);
+    }
+
+    /**
+     * Gets query for [[Service]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getService()
+    {
+        return $this->hasOne(Service::class, ['id' => 'service_id']);
+    }
+
+    /**
+     * Gets query for [[Status]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStatus()
+    {
+        return $this->hasOne(Status::class, ['id' => 'status_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+}
--- models/User.php
+++ models/User.php
@@ -2,103 +2,81 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $login
+ * @property string $password
+ * @property string $full_name
+ * @property string $phone
+ * @property string $email
+ * @property int $role_id
+ * @property string $auth_key
+ *
+ * @property Order[] $orders
+ * @property Role $role
+ */
+class User extends \yii\db\ActiveRecord
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
     /**
      * {@inheritdoc}
      */
-    public static function findIdentity($id)
+    public static function tableName()
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return 'user';
     }
 
     /**
      * {@inheritdoc}
      */
-    public static function findIdentityByAccessToken($token, $type = null)
+    public function rules()
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return [
+            [['login', 'password', 'full_name', 'phone', 'email', 'role_id', 'auth_key'], 'required'],
+            [['role_id'], 'integer'],
+            [['login', 'password', 'full_name', 'phone', 'email', 'auth_key'], 'string', 'max' => 255],
+            [['login'], 'unique'],
+            [['role_id'], 'exist', 'skipOnError' => true, 'targetClass' => Role::class, 'targetAttribute' => ['role_id' => 'id']],
+        ];
     }
 
     /**
-     * Finds user by username
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Login',
+            'password' => 'Password',
+            'full_name' => 'Full Name',
+            'phone' => 'Phone',
+            'email' => 'Email',
+            'role_id' => 'Role ID',
+            'auth_key' => 'Auth Key',
+        ];
+    }
+
+    /**
+     * Gets query for [[Orders]].
      *
-     * @param string $username
-     * @return static|null
+     * @return \yii\db\ActiveQuery
      */
-    public static function findByUsername($username)
+    public function getOrders()
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return $this->hasMany(Order::class, ['user_id' => 'id']);
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
+     * Gets query for [[Role]].
      *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
+     * @return \yii\db\ActiveQuery
      */
-    public function validatePassword($password)
+    public function getRole()
     {
-        return $this->password === $password;
+        return $this->hasOne(Role::class, ['id' => 'role_id']);
     }
 }
