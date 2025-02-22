80a7f8e67759ea7007c41939c0b499856567a04a
evghn <evghn94_git@mail.ru>
2025-02-22 09:46:56 +0300

    Настройка Yii2

--- config/db.php
+++ config/db.php
@@ -2,7 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    // у вас остается только закомментированная строка
+    // 'dsn' => 'mysql:host=localhost;dbname=de',
+
+    //только для osp6
+    'dsn' => 'mysql:host=MariaDB-11.2;dbname=de',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
