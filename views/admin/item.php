77221d5989fe126d953f9376f90de1dc37e73b46
evghn <evghn94_git@mail.ru>
2025-02-22 12:36:48 +0300

    Второй вариант управления заявками администратором

--- views/admin/item.php
+++ views/admin/item.php
@@ -43,6 +43,28 @@ use yii\bootstrap5\Html;
         : ''
       ?>
     </div>  
+
+    <!-- Вариант работы с заявками №2 -->
+    <div class='d-flex gap-5 justify-content-end mt-5'>
+      <?= Html::a('Просмотр', ['view', 'id' => $model->id], ['class' => 'btn btn-outline-info']) ?>
+
+
+      <!-- 
+      Новая -> «в работе» -> «выполнено»      
+      «B работе» -> «отмена»
+
+      -->
+      <?= $model->status_id == Status::getStatusId('Новая')
+        ? Html::a('В работу', ['work', 'id' => $model->id], ['class' => 'btn btn-outline-primary', 'data-method' => 'post', 'data-pjax' => 0])
+        : ''
+      ?>
+
+      <?= $model->status_id == Status::getStatusId('В работе')
+        ? Html::a('Отмена', ['cancel', 'id' => $model->id], ['class' => 'btn btn-outline-danger', 'data-method' => 'post', 'data-pjax' => 0])
+        . Html::a('Выполнено', ['apply', 'id' => $model->id], ['class' => 'btn btn-outline-success', 'data-method' => 'post', 'data-pjax' => 0])
+        : ''
+      ?>
+    </div>  
     
   </div>
 </div>
